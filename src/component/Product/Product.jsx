ef82e3e47017400ea1e677e2da9fbcd6c73c0003
SyedAlamin <syedalamin2580@gmail.com>
2023-03-27 02:36:40 +0600

    some data change

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+
 const Product = (props) => {
     const { img, price, name, seller, ratings } = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +17,10 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
